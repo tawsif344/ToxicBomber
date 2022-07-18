bc31a03e351b502d769c8b1240bdb6a39131648e
tawsif <84251628+tawsif344@users.noreply.github.com>
2022-07-18 22:05:26 +0600

    Update Tbomb.py

--- Tbomb.py
+++ Tbomb.py
@@ -62,8 +62,8 @@ def logo():
     print("\033[94m│                              \033[94m          │".center(columns+9))
     print("\033[94m│ \033[95mAuthor : ToxicNoob                     \033[94m│".center(columns+15))
     print("│ \033[95mTool   : Unlimited SMS Bomber          \033[94m│".center(columns+9))
-    print("│ \033[95mGitHub : https://github.com/Toxic-Noob \033[94m│".center(columns+9))
-    print("│ \033[95mCoder  : HunterSl4d3              \033[37mV3.0 \033[94m│".center(columns+15))
+    print("│ \033[95mGitHub : https://github.com/tawsif344/ToxicBomber \033[94m│".center(columns+9))
+    print("│ \033[95mCoder  : Tawsif              \033[37mV3.0 \033[94m│".center(columns+15))
     print("\033[94m└────────────────────────────────────────┘".center(columns+5))
 
 
